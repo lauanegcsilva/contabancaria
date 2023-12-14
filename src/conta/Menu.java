31cde6858be30a6b60f3e62b4fa6b810045a4742
Lauane Gonçalves <lauane.gcsilva@gmail.com>
2023-12-14 14:16:16 -0300

    feat: adiciona etapa 3

--- src/conta/Menu.java
+++ src/conta/Menu.java
@@ -1,12 +1,20 @@
 package conta;
 
 import java.util.Scanner;
+import conta.model.Conta;
 import conta.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
@@ -76,7 +84,7 @@ public class Menu {
 				
                 		break;
 			default:
-				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
                 		break;
 			}
 		}
